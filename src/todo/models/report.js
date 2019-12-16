cf8e243578a291003829f39dabe7e22b39a102a8
Pato Vargas <pevargas2730@gmail.com>
2019-12-16 01:22:25 -0600

    reports db model

---
+++ src/todo/models/report.js
@@ -0,0 +1,24 @@
+/*
+ Database model
+*/
+const mongoose = require('mongoose');
+try {
+  module.exports = mongoose.model('Report');
+} catch (error) {
+  const reportSchema = mongoose.Schema({
+    address: { type: String, required: true },
+    placeName: { type: String, required: true },
+    description: { type: String, required: true },
+    abuseType: { type: String, required: true },
+    dateOfEvent: { type: Date, required: true },
+    timeOfEvent: { type: String, required: true },
+    image: { type: String, required: false },
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true },
+    zipcode: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true }
+  });
+  module.exports = mongoose.model('Report', reportSchema);
+}
